d5f2ba3be7c73b3deadb9b7e34b1941a3721becd
Michael Wolthers (TIEVA) <148765204+MikeTIEVA@users.noreply.github.com>
2023-10-24 14:29:42 +0100

    Update AVD-AIB-OfficeUpdate.ps1
    check for running process
    wait for completion
    more detailed output

--- AVD-AIB-OfficeUpdate.ps1
+++ AVD-AIB-OfficeUpdate.ps1
@@ -1,20 +1,44 @@
 # AVD Update of installed Office Apps on avd image
 #
+$ExistingVersion = (Get-ItemProperty -path hklm:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration -Name VersionToReport).VersionToReport
 $errorState = 0
 $updatecmd =  "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe"
-$updatecmdParms = "/Update User"
+$updatecmdParms = "/Update User displaylevel=false"
+# Maximum wait time for offfice to update is 1 hour in seconds
+$MaxWait = 60*60
 try {
-    Start-Process -FilePath $updatecmd -ArgumentList $updatecmdParms -Wait
+    Start-Process -FilePath $updatecmd -ArgumentList $updatecmdParms
+    # Wait 10 seconds check for process running
+    Start-Sleep 10
+    $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+    do {
+        Start-Sleep 60
+        Write-Host "Checking .."
+        $UpdateProcessRunningStill = ((get-process "OfficeC2RClient" -ea SilentlyContinue) -ne $Null)
+        Write-Host "Status $U"
+    } while (($StopWatch.Elapsed.TotalSeconds -gt $MaxWait) -or ($true -eq $UpdateProcessRunningStill)) 
 }
 catch {
-    $errorState = 1
-    Write-Host "AVD AIB OfficeUpdate : Completed with errors"
+    $ErrorState = 1
+    Write-Host "AVD AIB OfficeUpdate : Completed with errors Unable to launch updater"
     Write-Host $_
     Write-Host $_.ErrorDetails
 
 }
 finally {
     if ($errorState -eq 0) {
-        Write-Host "AVD AIB OfficeUpdate : Update completed without errors"
+        if (-not $UpdateProcessRunningStill) {
+            $NewVersion = (Get-ItemProperty -path hklm:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration -Name VersionToReport).VersionToReport
+            Write-host "AVD-AIB-OfficeUpdate : Update Completed succesfully"
+            Write-host "                       $($StopWatch.Elapsed)"
+            Write-host "                       Version was : $ExistingVersion"
+            Write-Host "                       Version is  : $NewVersion"
+            Write-Host "AVD AIB OfficeUpdate : Update completed without errors"
+        }
+        else {
+            Write-host "AVD-AIB-OfficeUpdate : Update did NOT Complete"
+            $ErrrorState = 2
+        }
     }
 }
+Exit $ErrorState
