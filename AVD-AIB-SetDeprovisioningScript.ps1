49a8b1f79360723cea06b87c1511c518f16873f1
Chris Thompson (TIEVA) <148763475+ChrisTIEVA@users.noreply.github.com>
2024-06-25 21:15:37 +0100

    Update AVD-AIB-SetDeprovisioningScript.ps1

--- AVD-AIB-SetDeprovisioningScript.ps1
+++ AVD-AIB-SetDeprovisioningScript.ps1
@@ -1,3 +1,1 @@
-# This updates the C:\DeprovisioningScript.ps1 to utilise the /mode:vm to stop the "Please wait for the windows modules installer"
-
-((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit','Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
+%WINDIR%\system32\sysprep\sysprep.exe /generalize /shutdown /oobe /mode:vm
