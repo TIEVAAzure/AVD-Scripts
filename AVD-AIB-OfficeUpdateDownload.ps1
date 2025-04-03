3bbfe7e58c7fe2e492486599a0af0d81dd3409a3
Chris Thompson (TIEVA) <148763475+ChrisTIEVA@users.noreply.github.com>
2025-04-03 09:39:07 +0100

    Update AVD-AIB-OfficeUpdateDownload.ps1

--- AVD-AIB-OfficeUpdateDownload.ps1
+++ AVD-AIB-OfficeUpdateDownload.ps1
@@ -1,5 +1,5 @@
 # Define the URL of the remote PowerShell script (use the "raw" URL)
-$scriptUrl = "https://raw.githubusercontent.com/username/repo/branch/script.ps1"
+$scriptUrl = "https://raw.githubusercontent.com/TIEVAAzure/AVD-Scripts/refs/heads/main/AVD-AIB-OfficeUpdateV3.ps1"
 
 # Define a local path to save the downloaded script (e.g., in the TEMP folder)
 $scriptPath = "$env:TEMP\downloadedScript.ps1"
