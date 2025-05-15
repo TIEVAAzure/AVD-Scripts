e1e1d9feab7df72e6270a6e0a19f57dd397ccd57
Chris Thompson (TIEVA) <148763475+ChrisTIEVA@users.noreply.github.com>
2025-05-15 12:48:56 +0100

    Update AVD-AIB-OfficeUpdateV3.ps1
    Updated Timeout

--- AVD-AIB-OfficeUpdateV3.ps1
+++ AVD-AIB-OfficeUpdateV3.ps1
@@ -138,8 +138,8 @@ if ($installedVerObj -lt $onlineVerObj) {
     $updateCmdParms = "/Update User displaylevel=false"
     Start-Process -FilePath $updateCmd -ArgumentList $updateCmdParms
 
-    # Poll the registry for a version change (timeout after 10 minutes).
-    $maxWaitSeconds = 600
+    # Poll the registry for a version change (timeout after 15 minutes).
+    $maxWaitSeconds = 900
     $pollInterval = 20
     $elapsed = 0
     $NewVersionStr = $ExistingVersionStr
