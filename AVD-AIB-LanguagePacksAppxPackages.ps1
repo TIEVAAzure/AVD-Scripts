b8e9379a3acbd120137429c72059a038465aceab
Chris Thompson (TIEVA) <148763475+ChrisTIEVA@users.noreply.github.com>
2025-05-16 21:47:19 +0100

    Create AVD-AIB-LanguagePacksAppxPackages.ps1

---
+++ AVD-AIB-LanguagePacksAppxPackages.ps1
@@ -0,0 +1,2 @@
+Remove-AppxPackage -Package "Microsoft.LanguageExperiencePacken-GB_19041.62.219.0_neutral__8wekyb3d8bbwe" -AllUsers
+Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.LanguageExperiencePacken-GB_19041.62.219.0_neutral__8wekyb3d8bbwe"
