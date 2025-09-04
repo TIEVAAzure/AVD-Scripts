0eac50fb5fea73cb6846b9445cf9b1977755d476
Chris Thompson (TIEVA) <148763475+ChrisTIEVA@users.noreply.github.com>
2025-09-04 11:25:28 +0100

    Update AVD-AIB-ADHoc.ps1

--- AVD-AIB-ADHoc.ps1
+++ AVD-AIB-ADHoc.ps1
@@ -1,2 +1,1 @@
 Remove-AppxPackage -Package "Microsoft.LanguageExperiencePacken-GB_19041.62.219.0_neutral__8wekyb3d8bbwe" -AllUsers
-Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.LanguageExperiencePacken-GB_19041.62.219.0_neutral__8wekyb3d8bbwe"
