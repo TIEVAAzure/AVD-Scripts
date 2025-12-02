4403f59d15cdf5bf62f416d33d902fcb422deb14
markgiles1267 <mark.giles@tieva.co.uk>
2025-12-02 15:18:39 +0000

    Second Restructure
    AVD-Scripts
            Generic Scripts
    	Old Scripts
    	Customer-AVD
    		IMS
    			AVD-AIB-3rdParty-Download.ps1
    			AVD-AIB-AppInstalls.ps1 - Editable - Adding/Removing
    		BWG
    			AVD-AIB-3rdParty-Download.ps1
    			AVD-AIB-AppInstalls.ps1 - Editable - Adding/Removing
    	Applications
    		8x8.ps1
    		AdobeAcrobotReaderDC.ps1
    
    Also adding in the 3rd party script to download the  appintall script.

--- AVD-AIB-ADHoc.ps1
+++
@@ -1,2 +0,0 @@
-Remove-AppxPackage -Package "Microsoft.LanguageExperiencePacken-GB_19041.62.219.0_neutral__8wekyb3d8bbwe" -AllUsers
-Set-ItemProperty -Path "HKLM:\SYSTEM\Setup\Status\SysprepStatus" -Name "GeneralizationState" -Value 7
